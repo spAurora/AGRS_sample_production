226f7a71aa1434b8994d7ddd5127223418df9807
HaoyuWang <751984964@qq.com>
2023-04-24 18:02:54 +0200

    Update 3-shp_to_raster_multiclass.py

--- 3-shp_to_raster_multiclass.py
+++ 3-shp_to_raster_multiclass.py
@@ -22,11 +22,13 @@ import ogr
 
 os.environ['GDAL_DATA'] = r'C:\Users\75198\.conda\envs\learn\Lib\site-packages\GDAL-2.4.1-py3.6-win-amd64.egg-info\gata-data' #防止报error4错误
 
-image_path = r'E:\xinjiang_huyang_hongliu\Huyang_test_0808\1-clip_img_haze' #存储样本影像的文件夹
-line_path = r'E:\xinjiang_huyang_hongliu\Huyang_test_0808\1-artificial_shp_haze' #存储人工勾画矢量的文件夹
-save_path = r'E:\xinjiang_huyang_hongliu\Huyang_test_0808\1-raster_label_haze' #输出的矢量转栅格样本文件夹
+image_path = r'E:\xinjiang_huyang_hongliu\Huyang_test_0808\1-clip_img\1-clip_img_haze_lv3' #存储样本影像的文件夹
+line_path = r'E:\xinjiang_huyang_hongliu\Huyang_test_0808\1-artificial_shp\1-artificial_shp_haze_lv3' #存储人工勾画矢量的文件夹
+save_path = r'E:\xinjiang_huyang_hongliu\Huyang_test_0808\1-raster_label\1-raster_label_haze_lv3' #输出的矢量转栅格样本文件夹
 num_classes = 2 #类别数 不包含背景0
 
+if not os.path.exists(save_path):
+    os.mkdir(save_path)
 
 img_list = fnmatch.filter(os.listdir(image_path), '*.tif') # 过滤出所有tif文件
 
